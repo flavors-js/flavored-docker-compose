5f44b3fe619317c2baa88a5f905f412edfc58a52
Michael Ledin <m.ledin@appheads.ru>
2017-08-24 00:41:35 +0300

    fix(*): update flavors-cli to 1.2.1, add tests

--- test/index.js
+++ test/index.js
@@ -11,25 +11,15 @@ function testPath(...names) {
 const dockerComposeYml = 'docker-compose.yml';
 
 describe('flavored-docker-compose', () => {
-  it('collects docker-compose.yml files', () => {
+  it('collects docker-compose.yml files when `extends` is used', () => {
     const tp = (...names) => {
-      return testPath('collectsFiles', ...names);
+      return testPath('collectsFilesWithExtends', ...names);
     };
-    assert.deepEqual(require('flavors')('a-b', {transform: require('../transform'), workingDir: tp()}).dockerCompose.files, [
+    assert.deepEqual(require('flavors')('b-c', {transform: require('../transform'), workingDir: tp()}).dockerCompose.files, [
       tp(dockerComposeYml),
       tp('a', dockerComposeYml),
-      tp('a', 'b', dockerComposeYml)
-    ]);
-  });
-  it('collects docker-compose.yml files when configDirName is set', () => {
-    const tp = (...names) => {
-      return testPath('collectsFilesConfigDirName', ...names);
-    };
-    const n = 'config';
-    assert.deepEqual(require('flavors')('a-b', {configDirName: n, transform: require('../transform'), workingDir: tp()}).dockerCompose.files, [
-      tp(dockerComposeYml),
-      tp(n, 'a', dockerComposeYml),
-      tp(n, 'a', n, 'b', dockerComposeYml)
+      tp('b', dockerComposeYml),
+      tp('b', 'c', dockerComposeYml)
     ]);
   });
 });
